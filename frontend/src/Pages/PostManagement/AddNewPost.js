8e35233dc6bc43a506e577d62c9ce07707c6b712
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2025-04-26 14:17:50 +0530

    Add new learning paths

--- frontend/src/Pages/PostManagement/AddNewPost.js
+++ frontend/src/Pages/PostManagement/AddNewPost.js
@@ -129,7 +129,7 @@ function AddNewPost() {
               >
                 <option value="" disabled>Select Category</option>
                 <option value="AI and Data Science">AI and Data Science</option>
-                <option value="Painting">Painting</option>
+                <option value="Web Development">Web Development</option>
                 <option value="Jewelry making">Jewelry making</option>
                 <option value="Crochet">Crochet</option>
                 <option value="Other creative">Other creative</option>
