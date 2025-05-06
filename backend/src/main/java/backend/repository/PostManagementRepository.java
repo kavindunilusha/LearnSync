710c14e386c1e0f817c27aab0e0c69e707704e77
Akna <akna3bethmi@gmail.com>
2025-05-06 11:19:56 +0530

    Add annotations for PostManagementRepository

--- backend/src/main/java/backend/repository/PostManagementRepository.java
+++ backend/src/main/java/backend/repository/PostManagementRepository.java
@@ -2,7 +2,9 @@ package backend.repository;
 
 import backend.model.PostManagementModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostManagementRepository extends MongoRepository<PostManagementModel, String> {
     void deleteByUserID(String userID); // Ensure this method exists
 }
