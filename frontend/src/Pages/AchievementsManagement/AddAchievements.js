2351d5f92e1112dd7f60571f6820690f195f6d66
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2025-05-10 09:19:30 +0530

    modify add achievement.js

--- frontend/src/Pages/AchievementsManagement/AddAchievements.js
+++ frontend/src/Pages/AchievementsManagement/AddAchievements.js
@@ -130,11 +130,14 @@ function AddAchievements() {
                 required
               >
                 <option value="" disabled>Select Category</option>
-                <option value="Graphic Design">Graphic Design</option>
-                <option value="Web Development">Web Development</option>
-                <option value="Social Media">Social Media</option>
-                <option value="Mastering Time Management">Mastering Time Management</option>
-                <option value="Creative Writing for Beginners">Creative Writing for Beginners</option>
+                  <option value="AI and Data Science">AI and Data Science</option>
+                  <option value="Web Development">Web Development</option>
+                  <option value="Backend Development">Backend Development</option>
+                  <option value="DevOps">DevOps</option>
+                  <option value="Social Media">Social Media</option>
+                  <option value="Photograpya and Video Editing">Photograpy and Video Editing</option>
+                  <option value="Graphic Design">Graphic Design</option>
+                  <option value="Creative Writing for Beginner">Creative Writing for Beginners</option>
               </select>
             </div>
 
